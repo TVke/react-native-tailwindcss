10e9a7f8e5f8c03d8a424655102f5b61805561b0
Thomas <thomas@antwerpfactory.com>
2019-05-16 14:50:19 +0200

    Add `theme` as a extra accessor

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import color from './color';
 import colors from './color';
-import theme from './theme';
+import theme from './tailwind';
 import tailwind from './tailwind';
 import tw from './tailwind';
 import t from './tailwind';
