874479794a3f388f2888b31dac2f0f934236fb2a
Thomas <thomas_verhelst@telenet.be>
2020-01-13 13:12:09 +0100

    Update to improved path

--- util/configHandler.js
+++ util/configHandler.js
@@ -1,6 +1,6 @@
 import resolveConfig from 'tailwindcss/resolveConfig';
 
-let file = require('react-native-tailwindcss/stubs/simpleConfig.stub');
+let file = require('../stubs/simpleConfig.stub');
 
 try {
     file = require('../../../tailwind.config');
