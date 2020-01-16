ea84c866c12b573c6b6cda58894dfa80c83415f3
Thomas <thomas_verhelst@telenet.be>
2020-01-16 11:59:49 +0100

    Remove unused files

--- constants.js
+++
@@ -1,6 +0,0 @@
-import path from 'path';
-
-export const cli = 'RNTailwindcss';
-export const defaultConfigFile = './tailwind.config.js';
-export const defaultConfigStubFile = path.resolve(__dirname, './stubs/defaultConfig.stub.js');
-export const simpleConfigStubFile = path.resolve(__dirname, './stubs/simpleConfig.stub.js');
