5e17c48ae466e427432e802befcbf2f55d82cb5b
Thomas Schaaf <thomas.schaaf@komola.de>
2020-07-27 21:58:59 +0200

    Upgrade react-native version

--- __tests__/fixtures/testConfigHandler.js
+++ __tests__/fixtures/testConfigHandler.js
@@ -5,6 +5,7 @@ let file = require('../../stubs/simpleConfig.stub');
 try {
     file = require('./tailwind.config');
 } catch (ex) {
+    console.error('Loading tailwind config failed');
 }
 
 const {theme} = resolveConfig(file);
