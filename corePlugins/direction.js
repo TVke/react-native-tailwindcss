130c21cf233b173851acce977965d9fb86c03e20
tyler <tyler@skotchpine.com>
2020-11-14 14:59:57 -0600

    Update direction.js

--- corePlugins/direction.js
+++ corePlugins/direction.js
@@ -1,6 +1,6 @@
 import generator from '../util/generator';
 
-export default generator.generate('direction', 'direction', [
+export default generator.generate('direction', 'writingDirection', [
     'inherit',
     'ltr',
     'rtl',
